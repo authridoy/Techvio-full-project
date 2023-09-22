f92a6947e92a73b6c35a05538bd2d1001c0c5f49
authridoy <webkhridoy@gmail.com>
2023-09-22 10:01:00 +0600

    techvio lat commit

--- js/min.js
+++ js/min.js
@@ -30,6 +30,17 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+    },
   });
   
   //
